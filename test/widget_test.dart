aec6c54d47849445a0eb605cf527e708c975290b
Tushar Khari <tusharkhari@Tushars-MacBook-Air.local>
2024-06-29 16:54:28 +0530

    init

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:order_list_product_create/main.dart';
+import 'package:order_list_product_create/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
